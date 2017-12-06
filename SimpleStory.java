255ae696ae27895d69997e8c3b3c76acd2abfde7
Joel Chester <joel.chester@gmail.com>
2017-12-05 20:14:32 -0500

    Added file

---
+++ SimpleStory.java
@@ -0,0 +1,68 @@
+package JustSomeStupidStory;
+
+import java.util.Scanner;
+
+public class SimpleStory {
+
+	public static void main(String[] args) {
+		
+		Scanner userScan = new Scanner(System.in);
+		
+		/*  Some terms:
+	    noun - Person, place or thing
+	    verb - An action
+	    adjective - A description used to modify or describe a noun
+	    */
+		
+		System.out.print("How old are you? ");
+		userScan.nextLine();
+		int age = 17;
+		if (age < 17) {
+			System.out.print("Sorry you must be at least 17 to use this program.\n");
+		    System.exit(0);
+		} else { System.out.print("Well let's tell a story....\n");}
+		
+		
+		String name;
+		String verb;
+		String adjective;
+		String noun;
+		String adverb;
+		boolean isInvalidWord;
+		
+		System.out.print("Enter your name?  ");
+		name = userScan.nextLine();
+		
+		System.out.printf("Enter an adjective. ");
+		adjective = userScan.nextLine();
+		
+		do {
+	
+		System.out.printf("Enter an noun. ");
+		
+		noun = userScan.nextLine();
+		
+		isInvalidWord = (noun.equalsIgnoreCase("jerkface")||
+				noun.equalsIgnoreCase("dorkhead")||
+				noun.equalsIgnoreCase("nerdhead")||
+				noun.equalsIgnoreCase("geekface"));
+		if (isInvalidWord) {
+			System.out.print("That language is not allowed. Try again. \n\n");
+		}
+	 } while (isInvalidWord);
+		
+		System.out.printf("Enter an adverb. ");
+		adverb = userScan.nextLine();
+		
+		System.out.print("Enter a verb. ");
+		verb = userScan.nextLine();
+		
+		System.out.printf("Ok time for your simple story \n-------------------\n");
+		
+		System.out.printf("%s is a(n) %s %s. ", name, adjective, noun);
+		
+		System.out.printf("They are always %s %s. \n", adverb, verb);
+    
+	}
+
+}
